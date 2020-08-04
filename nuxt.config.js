9952162ad6f03194f5b6efba912dcff3275da388
Yoshihara Fumio <yoshihara@un-t.com>
2020-08-04 16:54:07 +0900

    fix: metaとOG変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,19 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'cashの変更テストページ',
+    title: 'タイトルを変更しました！！！',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'cashの変更をテストをしているページです。' },
-      { property: 'og:title', content: 'cashの変更テストページ' },
-      { property: 'og:description', content: 'cashの変更をテストをしているページです。' },
+      { hid: 'description', name: 'description', content: 'ディスクリプションを変更しました！！！' },
+      { property: 'og:title', content: 'タイトルを変更しました！！！' },
+      { property: 'og:description', content: 'ディスクリプションを変更しました！！！' },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: 'https://relaxed-dubinsky-8bf368.netlify.app/' },
       { property: 'og:image', content: 'https://relaxed-dubinsky-8bf368.netlify.app/og.png' },
       { property: 'twitter:card', content: 'summary_large_image' },
-      { property: 'twitter:title', content: 'cashの変更テストページ' },
-      { property: 'twitter:description', content: 'cashの変更をテストをしているページです。' },
+      { property: 'twitter:title', content: 'タイトルを変更しました！！！' },
+      { property: 'twitter:description', content: 'ディスクリプションを変更しました！！！' },
       { property: 'twitter:url', content: 'https://relaxed-dubinsky-8bf368.netlify.app/' },
       { property: 'twitter:image', content: 'https://relaxed-dubinsky-8bf368.netlify.app/og.png' }
     ],
